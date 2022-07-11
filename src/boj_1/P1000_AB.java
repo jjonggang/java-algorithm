6b2b596dca1bff91ab29a98dae929a3dbb6f0bc1
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-07-11 23:21:04 +0900

    sort_4

---
+++ src/boj_1/P1000_AB.java
@@ -0,0 +1,13 @@
+package boj_1;
+
+import java.util.Scanner;
+
+public class P1000_AB {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        input.split(" ");
+
+    }
+}
+
---
+++ src/boj_1/P2557_HelloWorld.java
@@ -0,0 +1,9 @@
+package boj_1;
+
+import java.util.Scanner;
+
+public class P2557_HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
